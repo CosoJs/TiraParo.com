19c057d8344ef2ed29cb69540adc621909aecbf7
ESTEBAN NAVA <esteban@prointernet.mx>
2024-10-31 01:15:45 -0600

    descargar pdf en finanzas

--- src/app/Components/invoice/invoice.component.ts
+++ src/app/Components/invoice/invoice.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-invoice',
@@ -14,5 +16,22 @@ export class InvoiceComponent {
 
   collapseSidebar() {
     this.isSidebarExpanded = false;
+  }
+
+  // Método para generar el PDF
+  downloadPDF() {
+    const invoice = document.getElementById('invoice-container'); // Elemento a capturar
+
+    if (invoice) {
+      html2canvas(invoice).then((canvas) => {
+        const imgData = canvas.toDataURL('image/png');
+        const pdf = new jsPDF('p', 'mm', 'a4');
+        const imgWidth = pdf.internal.pageSize.getWidth() - 20; // Ajusta el ancho de imagen
+        const imgHeight = (canvas.height * imgWidth) / canvas.width;
+
+        pdf.addImage(imgData, 'PNG', 10, 10, imgWidth, imgHeight);
+        pdf.save('invoice.pdf'); // Nombre del archivo PDF
+      });
+    }
   }
 }
